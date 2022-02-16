b1fb6354488f91887aa1c714be3bdcd98204608c
Nurizko Maulana <nurizko.maulana@gmail.com>
2022-02-17 02:32:51 +0700

    add Ig link

--- pages/home.js
+++ pages/home.js
@@ -1,5 +1,16 @@
-export default function Home(){
-    return (
-        <h1 className="text-center">home</h1>
-    )
+import Image from "next/image";
+export default function Home() {
+  return (
+    <>
+      <h1 className="text-center">home</h1>
+      <div className="w-24"><Image src="/images/membiru1.jpg" layout='fill'></Image></div>
+      {/* <Image src="/images/membiru2.jpg" layout='fill'></Image>
+      <Image src="/images/membiru3.jpg" layout='fill'></Image>
+      <Image src="/images/membiru4.jpg" layout='fill'></Image>
+      <Image src="/images/membiru5.jpg" layout='fill'></Image>
+      <Image src="/images/membiru6.jpg" layout='fill'></Image>
+      <Image src="/images/membiru7.jpg" layout='fill'></Image>
+      <Image src="/images/membiru8.jpg" layout='fill'></Image> */}
+    </>
+  );
 }
